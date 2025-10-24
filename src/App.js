2fb7066fbc0a264987207793ae1f85a0f170fa53
Filip Filipovic <102156199+RaptorFF@users.noreply.github.com>
2025-10-24 12:24:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,11 +30,33 @@ export default function App() {
 }
 
 function FriendList() {
+  const friends = initialFriends;
   return (
     <ul>
-      {initialFriends.map((friend) => (
-        <li>{friend.name}</li>
+      {friends.map((friend) => (
+        <Friend friend={friend} />
       ))}
     </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${friend.balance}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <button className="button">Select</button>
+    </li>
   );
 }
